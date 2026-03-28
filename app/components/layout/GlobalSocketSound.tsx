5780b7e85fda89defdcddef49636051dc6c3e539
shanialy <xhanialee1@gmail.com>
2026-03-28 06:01:54 +0500

    fix: global update

--- app/components/layout/GlobalSocketSound.tsx
+++ app/components/layout/GlobalSocketSound.tsx
@@ -6,21 +6,43 @@ import socketService from "@/app/services/socket.service";
 
 export default function GlobalSocketSound() {
   const user = useAppSelector((state) => state.auth.user);
-  console.log("GlobalSocketSound rendered, user:", user);
 
   useEffect(() => {
     if (!user) return;
 
-    const handleMessage = (message: any) => {
-      console.log("GlobalSocketSound received message:", message);
-      if (message.sender?._id !== user._id) {
-        const audio = new Audio("/faaah.mp3");
-        audio.play().catch(() => {});
-      }
+    const socket = socketService.getSocket();
+
+    const attachListener = () => {
+      console.log(
+        "GlobalSocketSound attaching listener, socket connected:",
+        socket?.connected,
+      );
+
+      const handleMessage = (message: any) => {
+        console.log("GlobalSocketSound received message:", message);
+        if (message.sender?._id !== user._id) {
+          const audio = new Audio("/faaah.mp3");
+          audio.play().catch(() => {});
+        }
+      };
+
+      socketService.on("message", handleMessage);
+
+      return () => {
+        console.log("GlobalSocketSound cleaning listener");
+        socketService.off("message", handleMessage);
+      };
     };
 
-    socketService.on("message", handleMessage);
-    return () => socketService.off("message", handleMessage);
+    if (!socket || !socket.connected) {
+      console.log("GlobalSocketSound waiting for socket connect...");
+      socket?.once("connect", () => {
+        console.log("GlobalSocketSound socket connected");
+        attachListener();
+      });
+    } else {
+      return attachListener();
+    }
   }, [user]);
 
   return null;
