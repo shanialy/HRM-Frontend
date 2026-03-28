439b5f8393c45e8c44b12764a6a9276c861c7884
shanialy <xhanialee1@gmail.com>
2026-03-28 05:16:21 +0500

    fix: update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import ReduxProvider from "./dashboard/providers/ReduxProvider";
 import SocketProvider from "./dashboard/providers/SocketProvider";
 import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { useAppSelector } from "./dashboard/redux/hooks";
+import socketService from "@/app/services/socket.service";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,26 +28,49 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
- return (
-  <html lang="en">
-    <body
-      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-    >
-      {/* ✅ REDUX PROVIDER */}
-      <ReduxProvider>
-        {/* ✅ SNACKBAR TOASTER */}
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            duration: 3000,
-          }}
-        />
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ReduxProvider>
+          <Toaster
+            position="top-center"
+            toastOptions={{
+              duration: 3000,
+            }}
+          />
 
-        <SocketProvider>
-          {children}
-        </SocketProvider>
-      </ReduxProvider>
-    </body>
-  </html>
-);
+          <SocketProvider>
+            <GlobalSocketSound />
+            {children}
+          </SocketProvider>
+        </ReduxProvider>
+      </body>
+    </html>
+  );
+}
+
+// ================= GLOBAL SOCKET SOUND =================
+function GlobalSocketSound() {
+  const user = useAppSelector((state) => state.auth.user);
+
+  useEffect(() => {
+    if (!user) return;
+
+    const handleMessage = (message: any) => {
+      if (message.sender?._id !== user._id) {
+        const audio = new Audio("/faaah.mp3");
+        audio.play().catch(() => {});
+      }
+    };
+
+    socketService.on("message", handleMessage);
+
+    return () => {
+      socketService.off("message", handleMessage);
+    };
+  }, [user]);
+
+  return null;
 }
