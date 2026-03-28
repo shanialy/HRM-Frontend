d534f514420b67b430a653ca6df3801085c4f20c
shanialy <xhanialee1@gmail.com>
2026-03-28 05:24:41 +0500

    fix: logs added

--- app/components/layout/GlobalSocketSound.tsx
+++ app/components/layout/GlobalSocketSound.tsx
@@ -6,11 +6,13 @@ import socketService from "@/app/services/socket.service";
 
 export default function GlobalSocketSound() {
   const user = useAppSelector((state) => state.auth.user);
+  console.log("GlobalSocketSound rendered, user:", user);
 
   useEffect(() => {
     if (!user) return;
 
     const handleMessage = (message: any) => {
+      console.log("GlobalSocketSound received message:", message);
       if (message.sender?._id !== user._id) {
         const audio = new Audio("/faaah.mp3");
         audio.play().catch(() => {});
